0f83a015de0e81dc2f34c4f2ed4d048d591c6e38
Don Delice Dushime <107407612+dondelice257@users.noreply.github.com>
2023-03-08 18:04:12 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent {
         name: 'Tecno Spark',
         icon: '<i class="fas fa-mobile-alt"></i>',
         price: 120,
-        quantity : 12
+        quantity : 13
       },
       {
         name: 'iPhone 13',
