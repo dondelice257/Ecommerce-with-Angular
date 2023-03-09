a436f62d9c5eff139bd30f40ed9fe8b984d3c6e8
Don Delice Dushime <107407612+dondelice257@users.noreply.github.com>
2023-03-09 18:52:24 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title: string=  'Shopping'
+  title: string= 'Shopping'
   isVisible :boolean =  false
   isAdded :boolean=  false
   list :any = [
